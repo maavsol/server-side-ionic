92cda87a4f23237dc143696c14b0c03fff28c560
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-29 13:32:06 +0200

    removing gitignore

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/pizzapp');
 const app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
