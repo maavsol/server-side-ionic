d72a47b9c32beac294dbc85b23a40af7cb30e6b9
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-30 10:34:25 +0200

    las direcciones se borran correctamente

--- routes/address-routes.js
+++ routes/address-routes.js
@@ -7,7 +7,7 @@ router.get('/findUserAddresses/:id', (req, res, next) => {
   const { id } = req.params;
   Address.find({ user: id })
     .then((foundAddresses) => {
-      res.status(500).json(foundAddresses);
+      res.status(200).json(foundAddresses);
     })
     .catch(e =>
       res.status(500).json({
@@ -23,12 +23,39 @@ router.post('/addNewAddress/:id', (req, res, next) => {
   address
     .save()
     .then((savedAddress) => {
-      res.status(500).json(savedAddress);
+      res.status(200).json(savedAddress);
     })
     .catch(e =>
       res.status(500).json({
         error: e.mesesage,
       }));
 });
+
+// edit address
+
+router.post('/editAddress/:id', (req, res, next) => {
+  const addressId = req.params.id;
+  const { streetName, floor, postalCode } = req.body.data;
+
+  const update = { streetName, floor, postalCode };
+
+  Address.findByIdAndUpdate(addressId, update, { new: true })
+    .then(updatedAddress => res.status(200).json(updatedAddress))
+    .catch(e =>
+      res.status(500).json({
+        error: e.mesesage,
+      }));
+});
+
+// delete address
+router.post('/deleteAddress/:id', (req, res, next) => {
+  Address.findByIdAndRemove(req.params.id)
+    .then(deletedAddress => res.status(200).json(deletedAddress))
+    .catch(e =>
+      res.status(500).json({
+        error: e.mesesage,
+      }));
+});
+
 
 module.exports = router;
