941cb98cfb17401642f787f8e0cd86256ad991b6
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-30 09:35:06 +0200

    find all user addresses by id

--- routes/address-routes.js
+++ routes/address-routes.js
@@ -3,16 +3,23 @@ const Address = require('../models/Address');
 
 const router = express.Router();
 
+router.get('/findUserAddresses/:id', (req, res, next) => {
+  const { id } = req.params;
+  Address.find({ user: id })
+    .then((foundAddresses) => {
+      res.status(500).json(foundAddresses);
+    })
+    .catch(e =>
+      res.status(500).json({
+        error: e.mesesage,
+      }));
+});
+
 router.post('/addNewAddress/:id', (req, res, next) => {
-   
   const user = req.params.id;
-
   const { streetName, postalCode, floor } = req.body;
-
   const addressData = { user, streetName, postalCode, floor };
-  console.log(addressData)
   const address = new Address(addressData);
-    
   address
     .save()
     .then((savedAddress) => {
