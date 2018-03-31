6be6e6cd71f15e3c8dfa594cac3d688607b684c6
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2018-03-31 15:55:44 +0200

    ya se pueden actualizar los orders pase lo q pase loco

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -43,9 +43,14 @@ router.get('/getMyOrder/:id', (req, res, next) => {
 });
 
 router.post('/updateOrderStatus/:id', (req, res, next) => {
-
-  const update = { status: 'accepted' };
-
+  const message = req.body.message;
+  let update;
+  if (message === 'accepted') {
+    update = { status: 'accepted' };
+  }
+  if (message === 'rejected') {
+    update = { status: 'rejected' };
+  }
   Order.findByIdAndUpdate(req.params.id, update, { new: true }).populate('addressToServe restaurant user')
     .then(updatedOrder => res.status(200).json(updatedOrder))
     .catch(e =>
